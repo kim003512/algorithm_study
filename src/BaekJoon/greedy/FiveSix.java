46db041bbe4f14ec88bcc24a3d7010d02be826d2
kim003512 <kim003512@naver.com>
2022-03-24 23:14:05 +0900

    greedy-5와 6의 차이

---
+++ src/BaekJoon/greedy/FiveSix.java
@@ -0,0 +1,24 @@
+package BaekJoon.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class FiveSix {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String a = st.nextToken().replace('6', '5');
+        String b = st.nextToken().replace('6', '5');
+
+        System.out.print(Integer.parseInt(a) + Integer.parseInt(b) + " ");
+
+        a = a.replace('5', '6');
+        b = b.replace('5', '6');
+
+        System.out.print(Integer.parseInt(a) + Integer.parseInt(b));
+
+    }
+}
